af54d96b507852e36f82e3a8601817c5e3b4928c
haejii <yanghagi@naver.com>
2022-04-09 23:17:33 +0900

    Implement first App Sectting

--- 1.FirstApp/1.FirstApp/ViewController.swift
+++ 1.FirstApp/1.FirstApp/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainLabel: UILabel!
+    @IBOutlet weak var MainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func MainButton(_ sender: Any) {
+        print("aaaaa")
+    }
+    
 }
 
