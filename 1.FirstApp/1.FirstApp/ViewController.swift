36ed32b6a8ccadbf9acb2058c1c19148135e8d66
haejii <yanghagi@naver.com>
2022-04-09 23:06:57 +0900

    first commit

---
+++ 1.FirstApp/1.FirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1.FirstApp
+//
+//  Created by 양혜지 on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
