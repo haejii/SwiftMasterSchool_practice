7571c305da6da084dc0106f0bc7c99fd4d820ec6
haejii <yanghagi@naver.com>
2022-04-12 23:01:47 +0900

    Finish Dice App

--- 2.DiceGame/2.DiceGame/ViewController.swift
+++ 2.DiceGame/2.DiceGame/ViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var imageList = ["black1", "black2", "black3", "black4", "black5", "black6"]
-
+  
     @IBOutlet weak var diceOne: UIImageView!
     @IBOutlet weak var diceTwo: UIImageView!
+    
+    
+    var imageList = ["black1", "black2", "black3", "black4", "black5", "black6"]
+    var diceArry : [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
