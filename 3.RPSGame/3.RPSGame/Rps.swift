55b5a2ef4b68424f83c37a1f44f0d2f3aa904b7f
haejii <yanghagi@naver.com>
2022-04-13 23:15:10 +0900

    Make Enum Type(RPS)

---
+++ 3.RPSGame/3.RPSGame/Rps.swift
@@ -0,0 +1,15 @@
+//
+//  Rps.swift
+//  3.RPSGame
+//
+//  Created by 양혜지 on 2022/04/13.
+//
+
+import Foundation
+
+enum RPS: Int {
+    case rock = 1
+    case scissors = 2
+    case paper = 3
+    
+}
--- 3.RPSGame/3.RPSGame/ViewController.swift
+++ 3.RPSGame/3.RPSGame/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var choiceLabel: UILabel!
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    
+    var ImageList : [UIImage] = [#imageLiteral(resourceName: "rock"), #imageLiteral(resourceName: "scissors"), #imageLiteral(resourceName: "paper")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.comImageView.image = #imageLiteral(resourceName: "ready")
+        self.myImageView.image = #imageLiteral(resourceName: "ready")
+        self.comChoiceLabel.text = "준비"
+        self.myChoiceLabel.text = "준비"
+        
     }
 
-
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        
+       // sender.titleLabel?.text == "가위"
+        sender.currentTitle!
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
